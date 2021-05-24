1d0782ed910e856b3c9d632404893a4f8b233f45
teles <teles@users.noreply.github.com>
2021-05-24 13:24:27 +0000

    deploy: 64e5ea6c1309cdc7d91959326026317067267643

--- _nuxt/static/1621861933/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-nuxt\u002F_nuxt\u002Fstatic\u002F1621861933",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fblog-nuxt\u002F",assetsPath:"\u002Fblog-nuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ffe5d978"}}}}(null));
---
+++ _nuxt/static/1621862666/saia-midi-plissada/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-nuxt\u002F_nuxt\u002Fstatic\u002F1621862666",layout:"post",error:a,serverRendered:true,routePath:"\u002Fsaia-midi-plissada",config:{_app:{basePath:"\u002Fblog-nuxt\u002F",assetsPath:"\u002Fblog-nuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c16addf8"}}}}(null));
