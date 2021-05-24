f94f6dd151c21143246906a1358a86e12210989e
teles <teles@users.noreply.github.com>
2021-05-24 13:57:49 +0000

    deploy: 00f23d1d316bcb42c9d1a00bfe2f5b5b78bc2deb

--- _nuxt/static/1621862666/pantalona-jeans/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-nuxt\u002F_nuxt\u002Fstatic\u002F1621862666",layout:"post",error:a,serverRendered:true,routePath:"\u002Fpantalona-jeans",config:{_app:{basePath:"\u002Fblog-nuxt\u002F",assetsPath:"\u002Fblog-nuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c16addf8"}}}}(null));
---
+++ _nuxt/static/1621864668/saia-midi-plissada/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-nuxt\u002F_nuxt\u002Fstatic\u002F1621864668",layout:"post",error:a,serverRendered:true,routePath:"\u002Fsaia-midi-plissada",config:{_app:{basePath:"\u002Fblog-nuxt\u002F",assetsPath:"\u002Fblog-nuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b1554182"}}}}(null));
