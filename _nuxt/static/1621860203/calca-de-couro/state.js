bc4a8d02db9aa8b510c307c1353e1838a00611f6
teles <teles@users.noreply.github.com>
2021-05-24 12:47:36 +0000

    deploy: 9e83ee1516255e5b049d0fa94b4172768d1dca2f

--- _nuxt/static/1621860203/calca-de-couro/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-nuxt\u002F_nuxt\u002Fstatic\u002F1621860203",layout:"post",error:a,serverRendered:true,routePath:"\u002Fcalca-de-couro",config:{_app:{basePath:"\u002Fblog-nuxt\u002F",assetsPath:"\u002Fblog-nuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a1bcf6be"}}}}(null));
---
+++ _nuxt/static/1621860455/calca-de-couro/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-nuxt\u002F_nuxt\u002Fstatic\u002F1621860455",layout:"post",error:a,serverRendered:true,routePath:"\u002Fcalca-de-couro",config:{_app:{basePath:"\u002Fblog-nuxt\u002F",assetsPath:"\u002Fblog-nuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3efa6449"}}}}(null));
